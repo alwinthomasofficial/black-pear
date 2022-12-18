c69a71012c25f245a39ea92afe30efd63ad535c6
Alwin Thomas <alwinthomasgt@outlook.com>
2022-12-18 01:29:43 +0000

    webpack issue fixed xd, delete pack-lock and do new npm i though before starting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Buffer } from 'buffer';
 
-import { BrowserRouter } from 'react-router-dom';
+// If we don't have a value for Buffer (node core module) create it/polyfill it
+if (window.Buffer === undefined) window.Buffer = Buffer;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/views/resultsPage.jsx
+++ src/views/resultsPage.jsx
@@ -6,6 +6,19 @@ const ResultsPage = () => {
   const location = useLocation();
   console.log('Test 1');
   console.log('data here', location.state);
+  const results = location.state;
+
+  const tableData = [];
+  results.forEach((result) => {
+    const { resource } = result;
+    tableData.push({
+      name: `${resource.name[0].given[0]} ${resource.name[0].family}`,
+      dateOfBirth: resource.birthDate,
+      nhsNumber: resource.identifier[0].value,
+    });
+  });
+  console.log('tableData here:', tableData);
+
   //   location.state &&
   //     location.state.data &&
   //     console.log('Route data', location.state.data);
